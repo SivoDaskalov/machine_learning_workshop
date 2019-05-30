708a3ebe352ab3c0ba34409fd6c63623aa319068
Sivo Daskalov <sivodaskalov@gmail.com>
2019-05-30 17:34:27 +0300

    Broad project structure defined

---
+++ datasets/__init__.py
@@ -0,0 +1,2 @@
+from .digits_dataset_loader import *
+from .synthetic_regression_dataset_generator import *
---
+++ feature_extraction/__init__.py
@@ -0,0 +1,2 @@
+from .digits_dataset_feature_extraction import *
+from .regression_dataset_preprocessing import *
