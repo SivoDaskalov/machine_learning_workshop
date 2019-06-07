0f0fb3362e904d53b818219816de9b79f1d60f31
Sivo Daskalov <sivodaskalov@gmail.com>
2019-06-07 13:47:28 +0300

    Example classification and regression model creation and evaluation added

--- tutoring/main.py
+++ tutoring/main.py
@@ -1,13 +1,14 @@
 from tutoring.wrangling import assemble
 from tutoring.regression import simple_linear_regression, enet_model_selection, enet_cv_model_selection
+from tutoring.classification import *
 
 X, y = assemble("boston")
-# model = simple_linear_regression(X, y, plot_figures = False)
-# model = enet_model_selection(X, y)
-# model = enet_cv_model_selection(X, y)
+model = simple_linear_regression(X, y, plot_figures = False)
+model = enet_model_selection(X, y)
+model = enet_cv_model_selection(X, y)
 
 X, y = assemble("breast_cancer")
-# model = train_and_eval_knn(X, y)
-# model = batch_train_models(X, y)
+model = train_and_eval_knn(X, y)
+model = batch_train_models(X, y)
 
 pass
